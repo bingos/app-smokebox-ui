3e7ebd90b591d9fa464d2a9d4a7ede5be1a66a2d
Chris Williams <chris@bingosnet.co.uk>
2009-05-19 23:37:16 +0100

    Made a start with coding

--- lib/App/SmokeBox/UI.pm
+++ lib/App/SmokeBox/UI.pm
@@ -6,6 +6,16 @@ use vars qw($VERSION);
 
 $VERSION = '0.02';
 
+use MooseX::POE::Role;
+
+has smokebox => ( 
+  is       => 'ro',
+  isa      => 'POE::Component::SmokeBox',
+  required => 1,
+);  
+
+no MooseX::POE::Role;
+
 'Smoking!';
 
 __END__
